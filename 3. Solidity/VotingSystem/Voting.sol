227f07c45402c14853454aea9c7c8326a9aa8e4a
RemOclock <remi.sulpice@oclock.io>
2022-06-12 13:03:49 +0200

    Mise en place des events

--- 3. Solidity/VotingSystem/Voting.sol
+++ 3. Solidity/VotingSystem/Voting.sol
@@ -6,4 +6,34 @@ import "@openzeppelin/contracts/access/Ownable.sol";
 
 contract Voting is Ownable {
 
+  // == Variables ==
+  struct Voter {
+      bool isRegistered;
+      bool hasVoted;
+      uint votedProposalId;
+  }
+  struct Proposal {
+      string description;
+      uint voteCount;
+  }
+  enum WorkflowStatus {
+      RegisteringVoters,
+      ProposalsRegistrationStarted,
+      ProposalsRegistrationEnded,
+      VotingSessionStarted,
+      VotingSessionEnded,
+      VotesTallied
+  }
+  // La valeur par défaut sera le premier élément de l'énumération => workflowStatus = WorkflowStatus.RegisteringVoters
+  // public pour avoir un getter automatiquement : pratique pour voir sur quelle phase on se trouve
+  WorkflowStatus public workflowStatus;
+  mapping(address=>Voter) whitelist;
+  Proposal[] proposals;
+  uint winnigProposalId;
+
+  // == Events ==
+  event VoterRegistered(address voterAddress); 
+  event WorkflowStatusChange(WorkflowStatus previousStatus, WorkflowStatus newStatus);
+  event ProposalRegistered(uint proposalId);
+  event Voted (address voter, uint proposalId);
 }
