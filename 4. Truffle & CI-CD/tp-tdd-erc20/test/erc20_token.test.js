a2a1a04b088a4a71694ca1b8996303ca66fe93cf
RemOclock <remi.sulpice@oclock.io>
2022-06-19 14:30:07 +0200

    Recup des premiers tests sur le contrat ERC20

--- 4. Truffle & CI-CD/tp-tdd-erc20/test/erc20_token.test.js
+++ 4. Truffle & CI-CD/tp-tdd-erc20/test/erc20_token.test.js
@@ -1,13 +1,54 @@
-const Erc20Token = artifacts.require("Erc20Token");
+// Libraire de fonctions utilitaires pour les tests de smart contracts
+const { BN } = require('@openzeppelin/test-helpers');
+// Fonction d'assertion 
+const { expect } = require('chai');
+const ERC20 = artifacts.require('Erc20Token');
 
-/*
- * uncomment accounts to access the test accounts made available by the
- * Ethereum client
- * See docs: https://www.trufflesuite.com/docs/truffle/testing/writing-tests-in-javascript
- */
-contract("Erc20Token", function (/* accounts */) {
-  it("should assert true", async function () {
-    await Erc20Token.deployed();
-    return assert.isTrue(true);
+contract('Erc20Token', (accounts) => {
+  // On commence par mettre en place nos données d'entrées
+  // Elles vont être utilisées pour nos tests
+  const _name = 'ALYRA';
+  const _symbol = 'ALY';
+  const _initialsupply = new BN(1000);
+  const _decimals = new BN(18);
+  const owner = accounts[0];
+  const recipient = accounts[1];
+  const spender = accounts[2];
+  
+  // un hook qui est trigger avant chaque test
+  // L'utilité ici est de faire nos tests avec un contrat fraichement deployé, clean et sans données résiduelles
+  beforeEach(async function() {
+  this.ERC20Instance = await ERC20.new(_initialsupply,{from: owner});
+  });
+  
+  it('a un nom', async function() {
+    expect(await this.ERC20Instance.name()).to.equal(_name);
+  });
+
+  it('a un symbole', async function() {
+    expect(await this.ERC20Instance.symbol()).to.equal(_symbol);
+  });
+
+  it('a une valeur décimal', async function() {
+    expect(await this.ERC20Instance.decimals()).to.be.bignumber.equal(_decimals);
+  });
+
+  it('vérifie la balance du propriétaire du contrat', async function() {
+    let balanceOwner = await this.ERC20Instance.balanceOf(owner);
+    let totalSupply = await this.ERC20Instance.totalSupply();
+    expect(balanceOwner).to.be.bignumber.equal(totalSupply);
+  });
+
+  it('vérifie si un transfer est bien effectué', async function() {
+    let balanceOwnerBeforeTransfer = await this.ERC20Instance.balanceOf(owner);
+    let balanceRecipientBeforeTransfer = await this.ERC20Instance.balanceOf(recipient);
+    let amount = new BN(10);
+  
+    await this.ERC20Instance.transfer(recipient, amount, {from: owner});
+    let balanceOwnerAfterTransfer = await this.ERC20Instance.balanceOf(owner);
+    let balanceRecipientAfterTransfer = await this.ERC20Instance.balanceOf(recipient);
+  
+    expect(balanceOwnerAfterTransfer).to.be.bignumber.equal(balanceOwnerBeforeTransfer.sub(amount));
+    expect(balanceRecipientAfterTransfer).to.be.bignumber.equal(balanceRecipientBeforeTransfer.add(amount));
   });
 });
