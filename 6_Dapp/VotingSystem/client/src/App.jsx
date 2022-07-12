838fbf6df5edffb4be7f49dab504a6e551a7ea00
Remitekky <remitekky@hotmail.fr>
2022-07-12 20:22:16 +0200

    Voter Panel - recup events VoterRegistered & affichage conditionnel

--- 6_Dapp/VotingSystem/client/src/App.jsx
+++ 6_Dapp/VotingSystem/client/src/App.jsx
@@ -3,21 +3,23 @@ import Intro from "./components/Intro/";
 import Setup from "./components/Setup";
 import Demo from "./components/Demo";
 import AdminPanel from "./components/AdminPanel";
+import VoterPanel from "./components/VoterPanel";
 import Footer from "./components/Footer";
 import "./App.css";
 
 function App() {
   return (
     <EthProvider>
-      <div id="App" >
+      <div id="App">
         <div className="container">
+          <AdminPanel />
+          <hr />
+          <VoterPanel />
           <Intro />
           <hr />
           <Setup />
           <hr />
           <Demo />
-          <hr />
-          <AdminPanel />
           <hr />
           <Footer />
         </div>
---
+++ 6_Dapp/VotingSystem/client/src/components/VoterPanel/index.jsx
@@ -0,0 +1,76 @@
+import { useState, useEffect } from "react";
+import { Segment, Header, Form, Button, Input } from "semantic-ui-react";
+import { useEth } from "../../contexts/EthContext";
+
+function VoterPanel() {
+  const {
+    state: { accounts, contract, artifact },
+  } = useEth();
+  const [isVoter, setIsVoter] = useState(false);
+  const [inputValue, setInputValue] = useState("");
+
+    useEffect(() => {
+      async function getVoter() {
+        if (artifact) {
+          let voters = await contract.getPastEvents("VoterRegistered", { fromBlock: 0, toBlock: "latest" });
+          const voter = voters.find((voter) => voter === accounts[0]);
+          if (voter) {
+            setIsVoter(true);
+          } else {
+            setIsVoter(false);
+          }
+        }
+      }
+
+      getVoter();
+    }, [accounts, contract, artifact]);
+
+  const handleChange = (evt) => {
+    setInputValue(evt.currentTarget.value);
+  };
+
+  return (
+    isVoter && (
+      <Segment raised size="huge" color="green">
+        <Header as="h2">Voter's panel</Header>
+
+        <Segment size="huge">
+          <Form>
+            <Form.Field>
+              <Input
+                value={inputValue}
+                onChange={handleChange}
+                icon="file alternate outline"
+                iconPosition="left"
+                placeholder="Add Proposal"
+                size="huge"
+                fluid
+              />
+            </Form.Field>
+            <Button color="green" type="submit" size="huge" fluid>
+              Add
+            </Button>
+          </Form>
+        </Segment>
+
+        <Segment size="huge">
+          <Form>
+            <Form.Field>
+              <Input list="proposal" placeholder="Chose a proposal" size="huge" />
+              <datalist id="proposal">
+                <option value="1">Lorem 1</option>
+                <option value="2">Lorem 2</option>
+                <option value="3">Lorem 3</option>
+              </datalist>
+            </Form.Field>
+            <Button color="green" type="submit" size="huge">
+              Add
+            </Button>
+          </Form>
+        </Segment>
+      </Segment>
+    )
+  );
+}
+
+export default VoterPanel;
