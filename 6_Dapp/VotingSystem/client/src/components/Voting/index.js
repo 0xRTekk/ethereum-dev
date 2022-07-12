0151198263980cd057adfae656ea9491ccafb0a7
Remitekky <remitekky@hotmail.fr>
2022-07-12 15:38:18 +0200

    Bouton entrer dans salle de vote pour solutionner contract = null

--- 6_Dapp/VotingSystem/client/src/components/Voting/index.js
+++ 6_Dapp/VotingSystem/client/src/components/Voting/index.js
@@ -1,6 +1,6 @@
 // == Imports npm
 import { useEffect, useState } from 'react';
-import { Header, Icon } from 'semantic-ui-react';
+import { Header, Icon, Button } from 'semantic-ui-react';
 
 // == Imports assets
 import './voting.css';
@@ -16,62 +16,34 @@ import Lists from '../Lists';
 
 // == Composant
 function Voting() {
-  const { state: { contract, accounts } } = useEth();
+  const { state: { accounts, contract } } = useEth();
 
+  const [isEntered, setIsEntered] = useState(false);
   const [isOwner, setIsOwner] = useState(false);
   const [isVoter, setIsVoter] = useState(false);
   const [voters, setVoters] = useState([]);
   const [isTallied, setIsTallied] = useState(false);
 
-  /*
-  Au montage  :
-  - On recup les voters via l'event VoterRegistered
-    => recup de tous les events passés et remplir une propriété du state
-  - On en profite pour se mettre en écoute des futurs events VoterRegistered
-    => On va remplir un tableau avec les voters à chaque émition de l'event puis rajouter dans le state
-  */
-  useEffect(() => {
-    async function getVoterRegistered() {
-      // On recup tous les events VoterRegistered passés du contrat
-      let votersList = await contract.getPastEvents('VoterRegistered', {
-        fromBlock: 0,
-        toBlock: 'latest',
-      });
+  const init = async () => {
+    setIsEntered(true);
 
-      // On se met en écoute des émissions d'events. On rajoute chaque nouvel event dans le state
-      contract.events.VoterRegistered({ fromBlock: 0 }).on('data', event => votersList.push(event));
-      setVoters(votersList);
-    };
+    // On check si l'account courant est l'owner du contract
+    const owner = await contract.methods.owner().call({ from: accounts[0] });
+    accounts[0] === owner ? setIsOwner(true) : setIsOwner(false);
 
-    getVoterRegistered();
-  }, []);
+    // On check si l'account courant est un voter
+    voters.find((voter) => voter === accounts[0]) ? setIsVoter(true) : setIsVoter(false);
 
-  /*
-  Au montage et lors du changement d'account :
-  - On recup l'owner du contract
-    => call d'une fonction view de Owner.sol et mémorisation dans une variable
-  - On recup les voters via l'event VoterRegistered
-    => recup de tous les events passés et remplir une propriété du state
+    // On recup tous les events VoterRegistered passés du contrat
+    let votersList = await contract.getPastEvents('VoterRegistered', {
+      fromBlock: 0,
+      toBlock: 'latest',
+    });
 
-  - Si l'account metamask courant correspond à l'owner
-    => Oui : isOwner true
-  - Autrement si l'account metamask courant est présent dans le tableau des voters
-    => isVoter true
-  */
-  useEffect(() => {
-    async function checkAccount() {
-      // On recup l'owner
-      const owner = await contract.methods.owner().call({ from: accounts[0] });
-      
-      // On check si l'account courant est owner
-      accounts[0] === owner ? setIsOwner(true) : setIsOwner(false);
-      
-      // On check si l'account courant est un voter
-      voters.find((voter) => voter === accounts[0]) ? setIsVoter(true) : setIsVoter(false);
-    };
-
-    checkAccount();
-  }, [accounts, voters]);
+    // On se met en écoute des émissions d'events. On rajoute chaque nouvel event dans le state
+    contract.events.VoterRegistered({ fromBlock: 0 }).on('data', event => votersList.push(event));
+    setVoters(votersList);
+  };
 
   return (
       <main className="voting">
@@ -84,10 +56,29 @@ function Voting() {
           <Icon name='cubes' />
           Blockchain Voting System
         </Header>
+
+        {!isEntered && 
+        (
+          <Button
+            animated
+            fluid
+            size='massive'
+            basic
+            color='orange'
+            onClick={init}
+          >
+            <Button.Content visible>Enter the voting room</Button.Content>
+            <Button.Content hidden>
+              <Icon name='arrow right' />
+            </Button.Content>
+          </Button>
+        )}
+
         {isOwner && <AdminPanel />}
         {isVoter && <VotersPanel />}
         {isTallied && <Winner />}
-        <Lists />
+        {isEntered && <Lists />}
+
       </main>
   );
 }
